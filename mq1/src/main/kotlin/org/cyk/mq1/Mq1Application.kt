464e45196fedf01e03d74e70094537bc19f45d24
cyk318 <chenyikang2022@163.com>
2024-05-04 15:21:53 +0800

    init

---
+++ mq1/src/main/kotlin/org/cyk/mq1/Mq1Application.kt
@@ -0,0 +1,11 @@
+package org.cyk.mq1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Mq1Application
+
+fun main(args: Array<String>) {
+    runApplication<Mq1Application>(*args)
+}
---
+++ mq1/src/test/kotlin/org/cyk/mq1/Mq1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.cyk.mq1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Mq1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
