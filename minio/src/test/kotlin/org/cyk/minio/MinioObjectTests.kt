3f127fce99987e56e9c1eb0111d0425c7ac499fb
cyk318 <chenyikang2022@163.com>
2024-07-06 17:44:11 +0800

    feat: minio 4

--- minio/src/test/kotlin/org/cyk/minio/MinioObjectTests.kt
+++ minio/src/test/kotlin/org/cyk/minio/MinioObjectTests.kt
@@ -2,11 +2,14 @@ package org.cyk.minio
 
 import io.minio.*
 import io.minio.errors.ErrorResponseException
+import io.minio.http.Method
 import jakarta.annotation.Resource
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import java.io.File
 import java.io.FileInputStream
+import java.util.concurrent.TimeUnit
+import kotlin.math.min
 
 @SpringBootTest
 class MinioObjectTests {
@@ -20,7 +23,7 @@ class MinioObjectTests {
         minioClient.putObject(
             PutObjectArgs.builder()
                 .bucket("dir1")
-                .`object`("test.gif")
+                .`object`("test2.gif")
                 //stream:
                 //第二个参数(long objectSize): 要上传的文件大小
                 //第三个参数(long partSize): 指定缓冲区的大小
@@ -57,6 +60,113 @@ class MinioObjectTests {
         }
 
     }
+
+    @Test
+    fun test4() {
+        val url = minioClient.getPresignedObjectUrl(
+            GetPresignedObjectUrlArgs.builder()
+                .bucket("dir1")
+                .`object`("test.gif")
+                .method(Method.GET) //指定生成的 url 请求方式
+                // .expiry(3, TimeUnit.SECONDS) 设置签名的过期时间
+                .build()
+        )
+        println(url)
+    }
+
+    @Test
+    fun test5() {
+        //新建文件夹 dir2
+        minioClient.makeBucket(
+            MakeBucketArgs.builder()
+                .bucket("dir2")
+                .build()
+        )
+
+        //设置文件夹的访问权限为 只读
+        val jsonConfig = """
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": [
+                    "*"
+                ]
+            },
+            "Action": [
+                "s3:GetBucketLocation"
+            ],
+            "Resource": [
+                "arn:aws:s3:::dir2"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": [
+                    "*"
+                ]
+            },
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::dir2"
+            ],
+            "Condition": {
+                "StringEquals": {
+                    "s3:prefix": [
+                        "*"
+                    ]
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": [
+                    "*"
+                ]
+            },
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::dir2/**"
+            ]
+        }
+    ]
+}
+        """.trimIndent()
+
+        minioClient.setBucketPolicy(
+            SetBucketPolicyArgs.builder()
+                .bucket("dir2")
+                .config(jsonConfig)
+                .build()
+        )
+
+        //上传图片
+        minioClient.uploadObject(
+            UploadObjectArgs.builder()
+                .bucket("dir2")
+                .`object`("test.gif")
+                .filename("D:/tmp/滑稽.gif")
+                .build()
+        )
+
+        val url = minioClient.getPresignedObjectUrl(
+            GetPresignedObjectUrlArgs.builder()
+                .bucket("dir2")
+                .`object`("test.gif")
+                .method(Method.GET)
+                .build()
+        ).split("?")[0] //这样就可以不需要 令牌 部分了
+        println(url)
+    }
+
 
 
 
